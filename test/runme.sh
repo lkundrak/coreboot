f6cdf303a9637d1c45707987e77d22fa0c930af8
Lubomir Rintel <lkundrak@v3.sk>
2015-06-06 18:55:01 +0200

    test

--- test/runme.sh
+++ test/runme.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
 
-cc -m32 -D__PRE_RAM__ -fno-builtin -lm -include src/include/kconfig.h -Ibuild -Isrc/arch/x86/include -Isrc/include -Isrc src/northbridge/intel/i965/raminit.c src/northbridge/intel/i965/ddr2.c src/northbridge/intel/i965/ram_calc.c test/emu.c -Wall -g3 -o test/emu
+cc -O0 -m32 -D__PRE_RAM__ -fno-builtin -include src/include/kconfig.h -Ibuild -Isrc/arch/x86/include -Isrc/include -Isrc src/northbridge/intel/i965/raminit.c src/northbridge/intel/i965/ddr2.c src/northbridge/intel/i965/ram_calc.c src/lib/clog2.c test/emu.c -Wall -g3 -o test/emu
 
-#valgrind --db-attach=yes test/emu test/dimms/[67]
-test/emu test/dimms/[67]
+#exec valgrind --db-attach=no test/emu test/dimms/[67]
+exec test/emu test/dimms/[67]
 
 #gcc -Isrc -Isrc/include -Ibuild -Isrc/device/oprom/include -include src/include/kconfig.h -Isrc/arch/x86/include -D__PRE_RAM__ -Os -pipe -g -nostdinc -nostdlib -Wall -Wundef -Wstrict-prototypes -Wmissing-prototypes -Wwrite-strings -Wredundant-decls -Wno-trigraphs -Wstrict-aliasing -Wshadow -Werror -fno-common -fno-builtin -m32 -Wl,-b,elf32-i386 -Wl,-melf_i386 -Wno-unused-but-set-variable -Wa,--divide -march=i686 src/northbridge/intel/i965/raminit.c
 
