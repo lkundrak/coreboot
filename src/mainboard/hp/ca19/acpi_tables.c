c1c2862cc2f2f2eb00a455ea00b779df29934179
Lubomir Rintel <lkundrak@v3.sk>
2017-02-27 13:47:18 +0100

    XXX add ACPI
    Change-Id: I3a0cdafc19159fe6c38e4dde08ad0bf2bd0dd6b8

---
+++ src/mainboard/hp/ca19/acpi_tables.c
@@ -0,0 +1,13 @@
+#include <arch/acpi.h>
+
+// src/mainboard/via/epia-m700/acpi_tables.c
+
+unsigned long acpi_fill_madt(unsigned long current)
+{
+	return current;
+}
+
+unsigned long acpi_fill_mcfg(unsigned long current)
+{
+	return current;
+}
---
+++ src/mainboard/hp/ca19/dsdt.asl
@@ -0,0 +1,10 @@
+DefinitionBlock(
+	"dsdt.aml",
+	"DSDT",
+	0x02,		// DSDT revision: ACPI v1.0
+	"COREv4",	// OEM id
+	"COREBOOT",	// OEM table id
+	0x20170227	// OEM revision
+)
+{
+}
