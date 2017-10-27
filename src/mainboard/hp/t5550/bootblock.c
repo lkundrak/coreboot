673fa97c3f0cb09c61d27d7c4c60ac8010499339
Lubomir Rintel <lkundrak@v3.sk>
2017-10-27 10:01:34 +0200

    Revert "bb"
    This reverts commit eef2050479ac767b318f102d4289f80b4b0f3b3d.

--- src/mainboard/hp/t5550/bootblock.c
+++
@@ -1,8 +0,0 @@
-#include <arch/io.h>
-
-static void bootblock_mainboard_init(void)
-{
-        //if (CONFIG_ROM_SIZE == 0x200000)
-	pci_io_write_config8(PCI_DEV(0, 0x11, 0), 0x40, 0x04);
-	pci_io_write_config8(PCI_DEV(0, 0x11, 0), 0x41, 0xc0);
-}
